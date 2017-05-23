46713eb533fa9fbfbbf382e0ab3ace8115e6d6ad
ngiusti <nicholas.giusti89@gmail.com>
2017-05-23 11:29:34 -0700

    moving based on time

---
+++ checkmod.py
@@ -0,0 +1,19 @@
+import os
+import time
+import shutil
+# this is stating the 24 hour period 
+past = (time.time() - 24*60*60)
+print past
+
+source = 'C:/Users/Nicholas/Desktop/2/'
+dest = 'C:/Users/Nicholas/Desktop/1/'
+
+
+files = os.listdir(source)
+
+for i in files:
+    if time.ctime(os.path.getmtime(source)) >= time.ctime(past):
+        shutil.move(source + i, dest)
+        print ("Files have been transfered")
+    else:
+        print ("Nothing New")
